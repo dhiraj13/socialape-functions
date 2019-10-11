13d3416872b2139ede51b861849066b9ac54f072
Dhiraj Khapangi <dkhapangi13@outlook.com>
2019-10-11 12:55:59 +0545

    User Registration

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,27 @@
 const functions = require('firebase-functions');
 var admin = require("firebase-admin");
+const app = require('express')();
 
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+const config = {
+    apiKey: "AIzaSyBHRniKXw7YOI8wZRKbh-KTkXV_4guDTUM",
+    authDomain: "socialape-744b5.firebaseapp.com",
+    databaseURL: "https://socialape-744b5.firebaseio.com",
+    projectId: "socialape-744b5",
+    storageBucket: "socialape-744b5.appspot.com",
+    messagingSenderId: "938088651727",
+    appId: "1:938088651727:web:d4b2041450692e5b5290ba",
+    measurementId: "G-VJC3CWNK3C"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(config);
+
+const db = admin.firestore();
 
 app.get('/screams', (req, res) => {
-    admin
-        .firestore()
+    db
         .collection('screams')
         .orderBy('createdAt', 'desc')
         .get()
@@ -19,23 +32,32 @@ app.get('/screams', (req, res) => {
                     screamId: doc.id,
                     body: doc.data().body,
                     userHandle: doc.data().userHandle,
-                    createdAt: doc.data().createdAt
+                    createdAt: doc.data().createdAt,
+                    commentCount: doc.data().commentCount,
+                    likeCount: doc.data().likeCount
                 });
             });
             return res.json(screams);
         })
-        .catch(err => console.error(err));
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: err.code });
+        });
 });
 
+// Post one scream
 app.post('/scream',(req, res) => {
+    if (req.body.body.trim() === '') {
+        return res.status(400).json({ body: 'Body must not be empty' });
+    }
+
     const newScream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
         createdAt: new Date().toISOString()
     };
 
-    admin
-        .firestore()
+    db
         .collection('screams')
         .add(newScream)
         .then(doc => {
@@ -47,6 +69,54 @@ app.post('/scream',(req, res) => {
         });
 });
 
-// https://baseurl.com/api/
+// Signup route
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle,
+    };
+
+    // TODO: validate data
+    let token, userId;
+    db
+        .doc(`/users/${newUser.handle}`).get()
+        .then(doc => {
+            if (doc.exists) {
+                return res.status(400).json({ handle: 'this handle is already taken' });
+            } else {
+                return firebase
+                            .auth()
+                            .createUserWithEmailAndPassword(newUser.email, newUser.password)
+                            .then()
+            }
+        })
+        .then(data => {
+            userId = data.user.uid;
+            return data.user.getIdToken();
+        })
+        .then(idtoken => {
+            token = idtoken;
+            const userCredentials = {
+                handle: newUser.handle,
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
+                userId
+            };
+            return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+        })
+        .then(() => {
+            return res.status(201).json({ token });
+        })
+        .catch(err => {
+            console.error(err);
+            if (err.code === 'auth/email-already-in-use') {
+                return res.status(400).json({ email: 'Email is already in use' })
+            } else {
+                return res.status(500).json({ error: err.code });
+            }
+        });
+});
 
 exports.api = functions.region('asia-east2').https.onRequest(app);
