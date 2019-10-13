a6a90db700a9f26e9b905a0d013423f81e6e9bbe
Dhiraj Khapangi <dkhapangi13@outlook.com>
2019-10-13 11:10:51 +0545

    Refactoring & Organising

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyBHRniKXw7YOI8wZRKbh-KTkXV_4guDTUM",
+    authDomain: "socialape-744b5.firebaseapp.com",
+    databaseURL: "https://socialape-744b5.firebaseio.com",
+    projectId: "socialape-744b5",
+    storageBucket: "socialape-744b5.appspot.com",
+    messagingSenderId: "938088651727",
+    appId: "1:938088651727:web:d4b2041450692e5b5290ba",
+    measurementId: "G-VJC3CWNK3C"
+};
