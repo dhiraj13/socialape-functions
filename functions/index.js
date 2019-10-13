53d1ebd3c441d4dccfde1612dfcb36f6e2cc094f
Dhiraj Khapangi <dkhapangi13@outlook.com>
2019-10-13 10:10:58 +0545

    Authentication Middleware

--- functions/index.js
+++ functions/index.js
@@ -45,15 +45,43 @@ app.get('/screams', (req, res) => {
         });
 });
 
+const FBAuth = (req, res, next) => {
+    let idToken;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    } else {
+        console.error('No token found');
+        return res.status(403).json({ error: 'Unauthorized' });
+    }
+
+    admin.auth().verifyIdToken(idToken)
+        .then(decodedToken => {
+            req.user = decodedToken;
+            console.log(decodedToken);
+            return db.collection('users')
+                .where('userId', '==', req.user.uid)
+                .limit(1)
+                .get();
+        })
+        .then(data => {
+            req.user.handle = data.docs[0].data().handle;
+            return next();
+        })
+        .catch(err => {
+            console.error('Error while verifying token', err);
+            return res.status(403).json(err);
+        });
+};
+
 // Post one scream
-app.post('/scream',(req, res) => {
+app.post('/scream', FBAuth, (req, res) => {
     if (req.body.body.trim() === '') {
         return res.status(400).json({ body: 'Body must not be empty' });
     }
 
     const newScream = {
         body: req.body.body,
-        userHandle: req.body.userHandle,
+        userHandle: req.user.handle,
         createdAt: new Date().toISOString()
     };
 
@@ -121,8 +149,8 @@ app.post('/signup', (req, res) => {
             userId = data.user.uid;
             return data.user.getIdToken();
         })
-        .then(idtoken => {
-            token = idtoken;
+        .then(idToken => {
+            token = idToken;
             const userCredentials = {
                 handle: newUser.handle,
                 email: newUser.email,
